7853e525a6da68fda50755676fadb1c90264c5b8
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-09-05 21:11:33 -0400

    :Done:

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, :content, presence: true
+  validates :content, length: {minimum: 100}
+  validates :category, inclusion: { in: %w(Non-Fiction Fiction),
+  message:"not a valid category"}
 end
